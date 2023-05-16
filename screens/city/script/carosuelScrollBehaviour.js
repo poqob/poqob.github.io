1e1f823dad7304510d537c05f010afe57e29eb54
Mustafa <mmermmus123@gmail.com>
2023-05-16 23:08:56 +0300

    city html re-created

---
+++ screens/city/script/carosuelScrollBehaviour.js
@@ -0,0 +1,14 @@
+// get carousel items
+var components = document.getElementsByClassName('carousel-item');
+
+// get sections in the page
+var sections = document.getElementsByClassName('page-section');
+
+// bind carousels with sections together
+for (let index = 0; index < components.length; index++) {
+    const element = components[index];
+    const sctn = sections[index];
+    element.addEventListener('click', function () {
+        sctn.scrollIntoView({ behavior: "smooth" });
+    });
+}
