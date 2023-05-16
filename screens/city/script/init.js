fc5b17d3c6fe7a979b9cb7d34b4b36165060aac7
Mustafa <mmermmus123@gmail.com>
2023-05-17 01:32:12 +0300

    city page update, init.js provides txt contents

---
+++ screens/city/script/init.js
@@ -0,0 +1,46 @@
+
+const p1 = "Bursa Teleferik, Türkiye'nin ilk teleferik hattıdır. 29 Ekim 1963'te açılmıştır. Bursa'nınşehir simgelerinden biri olan Bursa Teleferik aynı zamanda halatlı taşımacılık alanında 9 kmyatay uzunluğu ile Türkiye'nin en uzun teleferik hattıdır.";
+const p2 = "Bursa'nın Mustafakemalpaşa ilçesi sınırları içindedir ve ilçe merkezine 17 km uzaklıktadır.Bir fay hattının çökmesi sonucu oluşan Şelale, 38 metre yükseklikten dökülmektedir. Mesireve piknik alanı da bulunan Suuçtu Şelalesi, etrafını saran Kayın ağaçları ve serin havasıile tam bir oksijen deposudur.";
+const p3 = "Cumalıkızık, Türkiye'nin Bursa ilinin Yıldırım ilçesine bağlı bir mahalledir. Bursa şehirmerkezine 11 kilometredir. Ortalama 20 dakikada ulaşım sağlanır. Uludağ'ın kuzey eteklerindekurulmuş ve hâlen yaşayan beş Kızık köyünden biridir.";
+const h1 = 'Teleferik';
+const h2 = 'Suuçtu Şelalesi';
+const h3 = 'CumalıKızık';
+const sp1 = "Bursa Teleferik, Türkiye'nin ilk teleferik hattıdır. 29 Ekim 1963'te açılmıştır. Bursa'nın şehir simgelerinden biri olan Bursa Teleferik aynı zamanda halatlı taşımacılık alanında 9 km yatay uzunluğu ile Türkiye'nin en uzun teleferik hattıdır. İstasyonlarında bulunan eğlence merkezleri ve alışveriş dükkanları ile aynı zamanda bir yaşam merkezidir. Bünyesinde 4 istasyon barındıran Bursa Teleferik ilk istasyon olarak Güney Bursa'da yer alan Yıldırım ilçesine yakın olan Teferrüç mahallesindeki Teferrüç istasyonundan kalkmakta ve Uludağ'daki oteller bölgesine ulaşmaktadır. 2014'te İtalyan şirket Leitner Ropeways tarafından altyapısı yenilenen teleferik hattı Teleferik Holding A.Ş. tarafından işletilmektedir.TarihiCumhuriyet’in 40.yıl kutlamalarının yapıldığı 1963 yılında İsviçreli şirket Von Roll Holding tarafından ülkenin en büyük kayak merkezi Uludağ’a yapılan teleferik hattı 29 Ekim 1963’te açılmıştır. İnşaatı 1958'de başlayan hat 5 senede tamamlanmıştır.";
+const sp2 = "Suuçtu Şelalesi, Türkiye'nin Bursa ilinin Mustafakemalpaşa ilçesine 18 km uzaklıkta bulunan ve ilçenin içme suyu ihtiyacını karşılayan, 38 metre yükseklikteki felezlerden dökülen şelale.OluşumuSuuçtu Kaya ŞelalesiÇataltepe mevkiinde, Muradiyesarnıç köyü yakınlarında, Karadere altındaki fay hattının çökmesi ile oluşmuştur. Granit metamorfik kayalar üzerinden akan şelale 464 metre rakıma sahiptir. Ayrıca şelaleden 5 km boyunca Sünlük Mahallesine kadar küçük şelalelerin bulunduğu 3 doğal oluşumlu şelale bulunmaktadır.ÖzellikleriKayın, meşe, çam ağaçlarının gölgesinde serin bir dinlenme alanı olan bu alan, etrafındaki ahşap piknik masaları ve ocakları sayesinde piknikçilerin çok rağbet ettiği bir mesire yeridir. Suuçtu Şelalesi Şubat-Mart aylarında daha gür akmaktadır. Kış mevsiminde şelalenin genişliği 30 metreyi bulmaktadır. Sel olduğunda genişliği 50 metreye çıkabilmektedir. Tek parça halinde göle dökülen şelale bu sırada kayalara çarpmakta, suyu da bu vesileyle çok yükseklere kadar sıçramakta olup önündeki alçalan yamaçlara doğru ağaçların arasından uçmaktadır.TurizmTurizm bölgesi ilan edilen çevrede doğa sporları ve aktiviteler yapılmakta.11 Temmuz 2011 tarihinde Tabiat Parkı olarak tescillenmiştir. Tarım ve Orman Bakanlığı Doğa Koruma ve Milli Parklar Genel Müdürlüğü Suuçtu Tabiat Parkı yerli ve yabancı turistlerin yoğun ilgi gösterdiği doğa harikası eko park alanı.";
+const sp3 = "Osmanlılar'ın Bursa'da ilk yerleştikleri bölgelerden olan Cumalıkızık, 180'i halen kullanılan, bazılarında ise koruma ve restorasyon çalışmalarının yapıldığı toplam 270 ev ile Osmanlı Dönemi konut dokusunu günümüze taşımaktadır. Cumalıkızık yerleşiminin güneydoğusunda, Uludağ eteklerindeki Ihlamurcu Mevkii'nde, Bizans Devri'ne ait bir kilise kalıntısı 1969 yılında tespit edilmiştir. Kilise kalıntısının yüzeyde rastlanan bazı mimari parçaları Bursa Arkeoloji Müzesi'nde saklanmaktadır. Bursa yakınlarında kurulan Osmanlı Beyliği, kuruluşundan kısa zaman sonra bölgeye hakim olmayı başarmış, 1326 yılında Bursa'yı, 1331 yılında İznik'i fethederek yörede varlığını kesin olarak kabul ettirmiştir. Böylece Osmanlı halkının bu topraklara yerleşerek kentler ve köyler oluşturması sağlanmıştır. Cumalıkızık vakıf köyü olarak kurulmuştur ve bu özelliğini yerleşim dokusu, konut mimarisi ve yaşam biçimine yansıtmıştır. Uludağ’ın kuzeyindeki dik etekler ile vadilerin arasında sıkışıp kalan yöre köylerine, bu konumlarından dolayı ''kızık'' adı verilmiştir. Köylerin birbirlerinden ayrılması için de dereye yakın olanına Derekızık, Fidye verene Fidyekızık ve Kızık köylerinden topluca gidilerek cuma namazı kılınan köye de Cumalıkızık adları verilmiştir.Bursa'nın tarihi alanlarından Hanlar Bölgesi, Sultan Külliyeleri ve Cumalıkızık Köyü'nün, Unesco Dünya Miras Listesine alınmasına yönelik  Bursa Büyükşehir Belediyesi, Kültür ve Turizm Bakanlığı Kültür Varlıkları ve Müzeler Genel Müdürlüğü çalışmalarıyla hazırlanan dosya; 1 Şubat 2013 tarihi itibariyle, UNESCO Dünya Mirası Merkezi Sekreterliği'ne Dışişleri Bakanlığı aracılığıyla iletilmiştir. Cumalıkızık Köyü, UNESCO tarafından Haziran 2014 tarihinde Bursa’nın Hanlar Bölgesi, Sultan Külliyeleri ile birlikte UNESCO Dünya Miras Listesinde yer alması uygun görülmüştür.";
+const sh1 = 'Teleferik';
+const sh2 = 'Suuçtu Şelalesi';
+const sh3 = 'CumalıKızık Köyü';
+const pArr = [p1, p2, p3];
+const hArr = [h1, h2, h3];
+const spArr = [sp1, sp2, sp3];
+const shArr = [sh1, sh2, sh3];
+
+var pharagraphs = document.getElementsByClassName('.p');
+var headlines = document.getElementsByClassName('.h');
+var sectionsParagaphs = document.getElementsByClassName('.sp');
+var sectionsHeadlines = document.getElementsByClassName('.sh');
+
+// corosuel paragraph
+for (let index = 0; index < pharagraphs.length; index++) {
+    const element = pharagraphs[index];
+    element.textContent = pArr[index];
+}
+
+// corosuel headline
+for (let index = 0; index < headlines.length; index++) {
+    const element = headlines[index];
+    element.textContent = hArr[index];
+}
+
+//section headlines
+for (let index = 0; index < shArr.length; index++) {
+    const element = sectionsHeadlines[index];
+    element.textContent = shArr[index];
+}
+
+//section paragraph
+for (let index = 0; index < spArr.length; index++) {
+    const element = sectionsParagaphs[index];
+    element.textContent = spArr[index];
+}
